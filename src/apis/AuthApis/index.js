49f334ddc163fbdc628f47f8a7d70ddfcf402c6e
trinvanminh <minh.a1.no.0@gmail.com>
2022-08-21 02:00:15 +0700

    fake api

--- src/apis/AuthApis/index.js
+++ src/apis/AuthApis/index.js
@@ -1,7 +1,7 @@
 import axiosClient, { baseUrl } from "../AxiosClient";
 
 export const LoginApi = async (payload) => {
-  const url = `${baseUrl}/auth/login`;
+  const url = `${baseUrl}/api/auth/login`;
   try {
     const response = await axiosClient.post(url, payload);
     return response;
@@ -11,7 +11,7 @@ export const LoginApi = async (payload) => {
 };
 
 export const RegisterApi = async (payload) => {
-  const url = `${baseUrl}/auth/register`;
+  const url = `${baseUrl}/api/auth/register`;
   try {
     const response = await axiosClient.post(url, payload);
     return response;
@@ -21,7 +21,17 @@ export const RegisterApi = async (payload) => {
 };
 
 export const ResetPasswordApi = async (payload) => {
-  const url = `${baseUrl}/auth/reset-password`;
+  const url = `${baseUrl}/api/auth/reset-password`;
+  try {
+    const response = await axiosClient.post(url, payload);
+    return response;
+  } catch (err) {
+    return err;
+  }
+};
+
+export const ConfirmResetPasswordApi = async (payload) => {
+  const url = `${Apiurl}/api/auth/reset-password/confirm/${payload.uid}-${payload.token}/`;
   try {
     const response = await axiosClient.post(url, payload);
     return response;
