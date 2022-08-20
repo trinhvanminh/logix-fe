f41e36d4ac3d816bac18da8c6125cb8c7bacb123
trinvanminh <minh.a1.no.0@gmail.com>
2022-08-20 23:25:16 +0700

    ud

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,4 +1,4 @@
-import { Box, Container, Divider } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import React from "react";
 import FilterFilm from "../../components/FilterFilm";
 import MovieList from "../../components/MovieList";
